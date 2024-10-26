a452322d1036d60850a5b343de6462548ffffdd7
kiarra <beneciakiarra_2226240052@mhs.mdp.ac.id>
2024-10-26 09:24:00 +0700

    add : file
    menambahkan profile screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi1/screens/detail_screen.dart';
+import 'package:wisata_candi1/screens/profile_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ProfileScreen(),
+      // home: DetailScreen(candi: null,),
+    );
+  }
+}
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// TODO:1. Deklarasikan variabel yang dibutuhkan
+bool isSignedin=false;
+String fullName='';
+String userName='';
+int favoriteCandiCount=8;
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Stack(
+        children: [
+          Container(
+            height:200, width: double.infinity, color: Colors.deepPurple,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //TODO:2. Buat bagian ProfilHeader yang berisi gambar profil
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top:200-50),
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          decoration:  BoxDecoration(
+                    border: Border.all(color: Colors.deepPurple,width: 2),
+                            shape: BoxShape.circle,
+                                ),
+                          child: CircleAvatar(
+                            radius:50,
+                            backgroundImage: AssetImage('imahes/placeholder_image.png'),
+                          ),
+                        ),
+                        if(isSignedin)
+                          IconButton(
+                              onPressed: (){},
+                            icon:Icon(Icons.camera_alt, color: Colors.deepPurple[50],),),
+                      ],
+                    ),
+                  ),
+                )
+                //TODO:3. Buat bagian ProfilInfo yang berisi info profil
+                //TODO:4 Buat ProfileActions yang berisi TextButton sign in/out
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
