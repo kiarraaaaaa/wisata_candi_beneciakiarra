15e2ac0525b0e3e52b587775981d300b70b17ded
kiarraaaaaa <beneciakiarra_2226240052@mhs.mdp.ac.id>
2024-12-09 13:00:00 +0700

    fix
    fix coding

--- lib/main.dart
+++ lib/main.dart
@@ -19,104 +19,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'wisata candi',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
+        title: 'Take Picture',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
             iconTheme: IconThemeData(color: Colors.deepPurple),
             titleTextStyle: TextStyle(
-              color: Colors.deepPurple,
               fontSize: 20,
               fontWeight: FontWeight.bold,
-            )
+            ),
+          ),
+          colorScheme:
+          ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+            primary: Colors.deepPurple,
+            surface: Colors.deepPurple[50],
+          ),
+          useMaterial3: true,
         ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
-          primary: Colors.deepPurple,
-          surface: Colors.deepPurple[50],
-        ),
-        useMaterial3: true,
-      ),
-      // home: ProfileScreen(),
-      // home: DetailScreen(candi: candiList[0]),
-      home: SignInScreen(),
-      //  home: SignUpScreen(),
-      //  home: SearchScreen(),
-      // home: HomeScreen(),
-      // home: MainScreen(),
-      initialRoute: '/',
-      routes: {
-        '/homescreen': (context) => const HomeScreen(),
-        '/signin': (context) => SignInScreen(),
-        '/signup': (context) => SignUpScreen(),
-      },
+        home: const HomeScreen(title: 'Take Picture'),
+        initialRoute: '/',
+        routes: {
+          '/homescreen': (context) => const HomeScreen(title: 'Take Picture',),
+          '/Signin' : (context) => SignInScreen(),
+          '/Signup' : (context) => SignUpScreen(),
+
+        }
     );
   }
 }
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
+class MainSreen extends StatefulWidget { const MainSreen({super.key});
+@override
+State<MainSreen> createState() => _MainSreenState(); }
+class _MainSreenState extends State<MainSreen> {
   // TODO: 1. Deklarasikan variabel
   int _currentIndex = 0;
-  final List<Widget> _children = [
-    HomeScreen(),
-    SearchScreen(),
-    FavoriteScreen(),
-    ProfileScreen(),
-  ];
-
+  final List<Widget> _children = [ const HomeScreen(title: '',),
+    const SearchScreen(),
+    const FavoriteScreen(),
+    const ProfileScreen(), ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO: 2. Buat properti body berupa widget yang ditampilkan
+// TODO: 2. Buat properti body berupa widget yang ditampilkan
       body: _children[_currentIndex],
-      // TODO: 3. Buat properti bottomNavigationBar dengan nilai Theme
+// TODO: 3. Buat properti bottomNavigationBar dengan nilai Theme
       bottomNavigationBar: Theme(
-        // TODO: 4. Buat data dan Child dari Theme
-        data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple[50]),
-        child: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
+// TODO: 4. Buat data dan child dari Theme
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50], ),
+        child: BottomNavigationBar( currentIndex: _currentIndex, onTap: (index){
+          setState(() {
+            _currentIndex = index; });
+        },
           items: const [
             BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: Colors.deepPurple,
-              ),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.search,
-                color: Colors.deepPurple,
-              ),
+              icon: Icon(Icons.home, color: Colors.deepPurple,), label: 'Home',
+            ), BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.deepPurple,),
               label: 'Search',
             ),
             BottomNavigationBarItem(
-              icon: Icon(
-                Icons.favorite,
-                color: Colors.deepPurple,
-              ),
-              label: 'Favorite',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person,
-                color: Colors.deepPurple,
-              ),
-              label: 'Person',
+              icon: Icon(Icons.favorite, color: Colors.deepPurple,), label: 'Favorite',
+            ), BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.deepPurple,),
+              label: 'Profile',
             ),
           ],
-          selectedItemColor: Colors.deepPurple,
-          unselectedItemColor: Colors.deepPurple[100],
-          showSelectedLabels: true,
+          selectedItemColor: Colors.deepPurple, unselectedItemColor: Colors.deepPurple[100], showUnselectedLabels: true,
         ),
       ),
     );
--- lib/screens/SignInScreen.dart
+++ lib/screens/SignInScreen.dart
@@ -1,136 +1,181 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 class SignInScreen extends StatefulWidget {
-  SignInScreen({super.key});
+  const SignInScreen({super.key});
 
   @override
   State<SignInScreen> createState() => _SignInScreenState();
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  // TODO: 1. Deklarasikan variabel
   final TextEditingController _usernameController = TextEditingController();
-
   final TextEditingController _passwordController = TextEditingController();
 
   String _errorText = '';
-
   bool _isSignedIn = false;
-
   bool _obscurePassword = true;
+
+  Future<Map<String, String>> retrieveAndDecryptDataFromPrefs(
+      Future<SharedPreferences> prefs,
+      ) async {
+    final sharedPreferences = await prefs;
+    final encryptedUsername = sharedPreferences.getString('username') ?? '';
+    final encryptedPassword = sharedPreferences.getString('password') ?? '';
+    final keyString = sharedPreferences.getString('key') ?? '';
+    final ivString = sharedPreferences.getString('iv') ?? '';
+
+    final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+    final iv = encrypt.IV.fromBase64(ivString);
+
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final decryptedUsername = encrypter.decrypt64(encryptedUsername, iv: iv);
+    final decryptedPassword = encrypter.decrypt64(encryptedPassword, iv: iv);
+
+    // Mengembalikan data terdekripsi
+    return {
+      'username': decryptedUsername,
+      'password': decryptedPassword,
+    };
+  }
+
   void _signIn() async {
     try {
       final Future<SharedPreferences> prefsFuture =
       SharedPreferences.getInstance();
-      final String username = _usernameController.text;
-      final String password = _passwordController.text;
+      final String username = _usernameController.text.trim();
+      final String password = _passwordController.text.trim();
       print('Sign in attempt');
+
       if (username.isNotEmpty && password.isNotEmpty) {
         final SharedPreferences prefs = await prefsFuture;
-        final data = await _retrieveAndDecryptDataFromPrefs(prefs);
+        final data = await retrieveAndDecryptDataFromPrefs(
+            prefsFuture); //hanya prefs jika dimateri
         if (data.isNotEmpty) {
           final decryptedUsername = data['username'];
           final decryptedPassword = data['password'];
+
           if (username == decryptedUsername && password == decryptedPassword) {
-            _errorText = '';
-            _isSignedIn = true;
-            prefs.setBool('isSignedIn', true);
-            // Pemanggilan untuk menghapus semua halaman dalam tumpukan navigasi
+            setState(() {
+              _errorText = '';
+              _isSignedIn = true;
+              prefs.setBool('isSignedIn', true);
+            });
+
+            // Pemanggilan untuk menghapus halaman dalam tumpukan navigasi
             WidgetsBinding.instance.addPostFrameCallback((_) {
               Navigator.of(context).popUntil((route) => route.isFirst);
             });
-            // Sign in berhasil, navigasikan ke layar utama
+
+            // Sign in berhasil, navigasikan ke Layar Utama
             WidgetsBinding.instance.addPostFrameCallback((_) {
               Navigator.pushReplacementNamed(context, '/');
             });
             print('Sign in succeeded');
           } else {
+            setState(() {
+              _errorText = 'Nama pengguna atau kata sandi salah.';
+            });
             print('Username or password is incorrect');
           }
         } else {
-          print('No stored credentials found');
+          setState(() {
+            _errorText =
+            'Pengguna belum terdaftar, Silahkan daftar terlebih dahulu.';
+          });
+          print('Username or password cannot be empty');
         }
       } else {
-        print('Username and password cannot be empty');
-        // Tambahkan pesan untuk kasus ketika username atau password kosong
+        setState(() {
+          _errorText = 'Nama Pengguna dan kata sandi harus diisi.';
+        });
+        print('Username and password must be filled');
       }
     } catch (e) {
+      setState(() {
+        _errorText = 'An error occurred: $e';
+      });
       print('An error occurred: $e');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Todo: 2. pasang appbar
-      appBar: AppBar(title: Text('sign in'),),
-      // Todo: 3. pasang body
+      // TODO: 2. Pasang AppBar
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+      // TODO: 3. Pasang Body
       body: Center(
         child: SingleChildScrollView(
-          child : Padding(
+          child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
               child: Column(
-                mainAxisAlignment:  MainAxisAlignment.center,
+                // TODO: 4. Atur mainAxisAlignment dan crossAxisAlignment
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // Todo 5. pasang textForfield nama pengguna
+                  // TODO: 5. Pasang TextFormField Nama Pengguna
                   TextFormField(
                     controller: _usernameController,
                     decoration: InputDecoration(
-                      labelText: "nama pengguna",
+                      labelText: "Nama Pengguna",
                       border: OutlineInputBorder(),
                     ),
                   ),
+                  // TODO: 6. Pasang TextFormField Kata Sandi
                   SizedBox(height: 20),
                   TextFormField(
                     controller: _passwordController,
                     decoration: InputDecoration(
-                      labelText: "kata sandi",
+                      labelText: "Kata Sandi",
                       errorText: _errorText.isNotEmpty ? _errorText : null,
                       border: OutlineInputBorder(),
                       suffixIcon: IconButton(
-                        onPressed: (){
+                        onPressed: () {
                           setState(() {
                             _obscurePassword = !_obscurePassword;
                           });
                         },
                         icon: Icon(
-                          _obscurePassword ? Icons.visibility_off
+                          _obscurePassword
+                              ? Icons.visibility_off
                               : Icons.visibility,
-                        ),),
+                        ),
+                      ),
                     ),
                     obscureText: _obscurePassword,
                   ),
+                  // TODO: 7. Pasang ElevatedButton Sign In
                   SizedBox(height: 20),
                   ElevatedButton(
-                    onPressed: (){},
-                    child: Text("sign in"),),
-                  SizedBox(height: 10),
-                  // TextButton(
-                  //     onPressed: (){},
-                  //     child: Text("Belum punya akun? Daftar di sini."),
-                  // ),
+                    onPressed: _signIn,
+                    child: Text('Sign In'),
+                  ),
+                  // TODO: 8. Pasang RichText Sign Up
                   SizedBox(height: 10),
                   RichText(
                     text: TextSpan(
-                        text: "Belum punya akun?",
-                        style: TextStyle(fontSize: 16, color: Colors.deepPurple
+                      text: 'Belum punya akun? ',
+                      style: TextStyle(fontSize: 16, color: Colors.deepPurple),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Daftar disini.',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                            fontSize: 16,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.pushNamed(context, '/signup');
+                            },
                         ),
-                        children:  <TextSpan>[
-                          TextSpan(
-                            text: "Daftar di sini.",
-                            style: TextStyle(
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline,
-                                fontSize: 16
-                            ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.pushNamed(context, '/signup');
-                              },
-
-                          )
-                        ]
+                      ],
                     ),
                   ),
                 ],
@@ -140,6 +185,5 @@ class _SignInScreenState extends State<SignInScreen> {
         ),
       ),
     );
-
   }
 }
